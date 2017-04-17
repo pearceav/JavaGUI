bffb8ac33373e0d0c35c4029597fe4784443efa9
Amanda Pearce <Amanda Pearce>
2017-04-17 15:08:31 -0400

    added BLUE buttom to GUIDemo, it changes the background of the pannel color to blue when you click it.

--- GUIDemo.java
+++ GUIDemo.java
@@ -1,6 +1,7 @@
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.*;
 
 import javax.swing.*;
 
@@ -15,6 +16,7 @@ public class GUIDemo extends JFrame
     private JPanel panel;
     private JButton biggerButton;
     private JButton smallerButton;
+    private JButton blueButton;   // Changes color to blue
 
     /**
      * Set up the application.
@@ -22,17 +24,20 @@ public class GUIDemo extends JFrame
     public GUIDemo()
     {
  // CONSTRUCTOR NEEDS TO BE FINISHED!
-        setTitle("Bigger/Smaller");
-        setSize(200, 100);
+        setTitle("Bigger/Smaller/Blue");
+        setSize(250, 150);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panel = new JPanel();
         biggerButton = new JButton("BIGGER");
         smallerButton = new JButton("SMALLER");
+        blueButton = new JButton("BLUE");
         biggerButton.addActionListener(new ButtonHandler());
         smallerButton.addActionListener(new ButtonHandler());
+        blueButton.addActionListener(new ButtonHandler());
         add(panel);
         panel.add(biggerButton);
         panel.add(smallerButton);
+        panel.add(blueButton);
         setVisible(true);
     }
 
@@ -53,6 +58,10 @@ public class GUIDemo extends JFrame
             if (e.getSource().equals(biggerButton))
             {
                 setSize(size.width + 10, size.height + 10);
+            } 
+            else if(e.getSource().equals(blueButton))
+            {
+                panel.setBackground(Color.BLUE);
             }
             else
             {
